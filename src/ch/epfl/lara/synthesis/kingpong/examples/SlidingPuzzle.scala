77f1a2d00f6ee9a2a740a0eb03c5327e3650f7a3
Lomig Megard <lomig.megard@epfl.ch>
2014-04-09 15:28:47 +0200

    Updated SlidingPuzzle game. Now the winning condition is working.

--- src/ch/epfl/lara/synthesis/kingpong/examples/SlidingPuzzle.scala
+++ src/ch/epfl/lara/synthesis/kingpong/examples/SlidingPuzzle.scala
@@ -19,31 +19,32 @@ class SlidingPuzzle extends Game {
   val wins = Category("Wins")()
   
   val gameboard = array(arrays)("MyArray", x = 0, y = 0, columns = 3, rows = 3)
-  val win = intbox(wins)("win", x = -1, y = -3, visible = false)
+  val win = booleanbox(wins)("win", x = -1, y = -3, value = false)
   
   // Pieces
-  for (col <- 0 until 3; row <- 0 until 3 if !(col == 0 && row == 0)) {
+  val piecesObj = for (i <- 0 until 8) yield {
+    val col = i % 3
+    val row = i / 3
     rectangle(pieces)(
       name = "piece[" + col + "," + row + "]", 
       x = gameboard.cells(col)(row).x, 
       y = gameboard.cells(col)(row).y,
       color = colorful(col))
   }
-  
+    
   // Borders
   val top    = rectangle(borders)("top", x = 0.25, y = -1.75, width = 3.5, height = 0.5)
   val right  = rectangle(borders)("right", x = 1.75, y = 0.25, width = 0.5, height = 3.5)
   val bottom = rectangle(borders)("bottom", x = -0.25, y = 1.75, width = 3.5, height = 0.5)
   val left   = rectangle(borders)("left", x = -1.75, y = -0.25, width = 0.5, height = 3.5)
-
   
   // Moving pieces rule
   val r1 = foreach(pieces) { piece =>
     fingerMoveOver(piece) { move => Seq(
       //debug("Move by %s.", move.expr),
-      piece.velocity := (move._2 - move._1) * 10,
-      piece.x += move._2._1 - move._1._1,
-      piece.y += move._2._2 - move._1._2
+      piece.velocity := (move._2 - move._1) * 15
+//      piece.x += move._2._1 - move._1._1,
+//      piece.y += move._2._2 - move._1._2
     )}
   }
   
@@ -54,14 +55,18 @@ class SlidingPuzzle extends Game {
     )
   }
   
-  //TODO Winning condition rule
-  val r3 = If(forall(gameboard.cellsCategory) { cell =>
-    If(Row(cell) =:= 0 && Column(cell) =:= 0, 
-        true, 
-        false)
-  },
-    win.visible := true,
-    win.visible := false
+  // Winning condition rule
+  val r3 = If(
+    Contains(gameboard.cells(0)(0), piecesObj(0)) &&
+    Contains(gameboard.cells(1)(0), piecesObj(1)) &&
+    Contains(gameboard.cells(2)(0), piecesObj(2)) &&
+    Contains(gameboard.cells(0)(1), piecesObj(3)) &&
+    Contains(gameboard.cells(1)(1), piecesObj(4)) &&
+    Contains(gameboard.cells(2)(1), piecesObj(5)) &&
+    Contains(gameboard.cells(0)(2), piecesObj(6)) &&
+    Contains(gameboard.cells(1)(2), piecesObj(7)),
+    win.value := true,
+    win.value := false
   )
  
   register(r1)
