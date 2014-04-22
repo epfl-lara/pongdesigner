ed44afaae97520117224415b0ab3f44012833c00
Lomig Megard <lomig.megard@epfl.ch>
2014-04-22 11:52:30 +0200

    Removed cassowary as it is currently unused and increases compilation time.

--- src/ch/epfl/lamp/cassowary/EditInfo.scala
+++
@@ -1,4 +0,0 @@
-package ch.epfl.lamp.cassowary
-
-final class EditInfo(val constraint: Constraint, val clvEditPlus: SlackVar,
-    val clvEditMinus: SlackVar, var prevEditConstant: Double, val index: Int)
